a902c25e81a8e1cc8d671064a49e7e3e46e2d120
Kevin Anderson <kmichael419@hotmail.com>
2020-07-17 16:10:48 -0500

    get up and running and images are now rendering to the page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList';
 import GalleryItem from '../GalleryItem/GalleryItem';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    gallery: [],
+  };
+
+  componentDidMount() {
+    console.log('READY');
+    this.getGallery();
+  }
+
+  getGallery() {
+    Axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        console.log('response', response.data);
+        this.setState({
+          gallery: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log('error during get', error);
+      });
+  }
+
   render() {
+    const displayGallery = this.state.gallery.map((item, index) => {
+      return (
+        <div key={item.id}>
+          <img src={item.path}></img> {item.likes}
+        </div>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br />
-        <GalleryList />
+        {displayGallery}
+        {/* <GalleryList /> */}
         <GalleryItem />
-        <img src="images/goat_small.jpg" />
+        {/* <img src="images/goat_small.jpg" /> */}
       </div>
     );
   }
