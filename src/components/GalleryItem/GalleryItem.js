074c024442aad5d1cff4270ca8cf50ccb5029ff2
Kevin Anderson <kmichael419@hotmail.com>
2020-07-17 23:06:57 -0500

    clicking image now switches image to description

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      clicked: true,
+    });
+    console.log('button clicked');
+  };
+
   render() {
-    return (
-      <div>
-        <img alt={this.props.item.description} src={this.props.item.path}></img>
-        <button type="button">Love This!</button>
-        <div className="likesDiv">{this.props.item.likes}</div>
-        {/* <div>{this.props.item}</div> */}
-      </div>
-    );
+    if (this.state.clicked === true) {
+      return (
+        <div>
+          <p className="descriptionText">{this.props.item.description}</p>
+          <button onClick={this.handleClick} type="button">
+            Love This!
+          </button>
+          <div className="likesDiv">{this.props.item.likes}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <img
+            onClick={this.handleClick}
+            alt={this.props.item.description}
+            src={this.props.item.path}
+          ></img>
+          <button type="button">Love This</button>
+          <div className="likesDiv">{this.props.item.likes}</div>
+        </div>
+      );
+    }
   }
 }
 
